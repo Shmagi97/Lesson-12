58b0c3497608e67cb5d2ef9337d57df3934f6e15
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-07-03 21:37:16 +0400

    jacascript function pages
     ar aris dasrulebuli

--- homeinternal.js
+++ homeinternal.js
@@ -1,14 +1,11 @@
-window.addEventListener("message", (event) => {
-  if (event.data == "ChangeElement") {
-    const ElementToChange = document.querySelectorAll(
-      ".hom-internal-foto-3, .hom-internal-foto-2, .hom-internal-foto-1"
-    );
-
-    ElementToChange.forEach((elements) => {
-      elements;
-    });
-  }
-});
+// window.addEventListener("message", (event) => {
+//   console.log("message");
+//   if (event.data == "ChangeElement") {
+//     const ElementToChange = document.querySelectorAll(
+//       ".hom-internal-foto-3, .hom-internal-foto-2, .hom-internal-foto-1"
+//     );
+//   }
+// });
 
 // const ElementToChange = document.getElementById("hominternalfoto3.js");
 // const cda = document.getElementById("had-line-end-element-3.js");
@@ -16,3 +13,22 @@ window.addEventListener("message", (event) => {
 // cda.addEventListener("click", () => {
 //   ElementToChange.style.visibility = "visible";
 // });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const ImageToChange = document.getElementById("hominternalfoto3.js");
+  window.addEventListener("message", (event) => {
+    if ((event.data = "ChangeElement")) {
+      ImageToChange.style.zIndex = "1";
+    }
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const ImageToChange3 = document.getElementById("hominternalfoto2.js");
+  window.addEventListener("message", (event) => {
+    if ((event.data = "ChangeElement3")) {
+      ImageToChange3.style.zIndex = "13";
+      console.log("gamovida meore ");
+    }
+  });
+});
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const MeoreFoto = document.getElementById("MeoreContFoto2.js");
-// const MeorefotoHominternal = document.getElementById("hom-internal-foto-3.js");
+// const MeoreFoto = document.getElementById("MeoreContFoto2.js");
+// // const MeorefotoHominternal = document.getElementById("hom-internal-foto-3.js");
 
-MeoreFoto.addEventListener("click", () => {
-  const SamizneFaili = window.location.origin + "/HomeInternal.html";
-  window.postMessage("ChangeElement", SamizneFaili);
+// MeoreFoto.addEventListener("click", () => {
+//   const SamizneFaili = window.location.origin + "/HomeInternal.html";
+//   window.postMessage("ChangeElement", SamizneFaili);
+// });
+
+const ImageLink = document.getElementById("MeoreContFoto2.js");
+const ImageLink3 = document.getElementById("MesameContFoto2.js");
+
+ImageLink.addEventListener("click", (event) => {
+  event.preventDefault();
+  const TargetWindow = window.open("HomeInternal.html");
+  TargetWindow.onload = () => {
+    TargetWindow.postMessage("ChangeElement", "*");
+  };
+});
+
+ImageLink3.addEventListener("click", (event) => {
+  event.preventDefault();
+  const TargetWindow3 = window.open("HomeInternal.html");
+  TargetWindow3.onload = () => {
+    TargetWindow3.postMessage("ChangeElement3", "*");
+  };
 });
